9112f3b397a1c32db350c81e4362a5ca9a0d2bda
olaldiko <gorka.olalde@gmail.com>
2016-03-07 21:27:56 +0100

    added second exercise of JUnit

---
+++ JUnitTest2/src/main/java/MyClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by gorkaolalde on 7/3/16.
+ */
+    public class MyClass {
+        public static void suma() {
+            Scanner scanner = new Scanner(System.in);
+            int primerSumando = scanner.nextInt();
+            int segundoSumando = scanner.nextInt();
+            System.out.print(primerSumando+segundoSumando);
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
